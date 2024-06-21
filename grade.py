b0c6c4a1de3ae9bac30ad5395aa15267d2ee82ea
Ozzy <asfandyar424@hotmail.com>
2024-06-20 21:08:05 -0500

    Create grade.py

---
+++ grade.py
@@ -0,0 +1,28 @@
+score = int(input("Score: "))
+
+if score >= 90:
+  print("Grade : A")
+elif score >= 80:
+  print("Grade : B")
+elif score >= 70:
+  print("Grade : C")
+elif score >= 60:
+  print("Grade: D")
+else:
+  print("Grade : F")
+
+
+
+#Old code
+#score = int(input("Score: "))
+#
+#if 90 <= score and score <= 100:
+#  print("Grade : A")
+#elif score >= 80 <= score and score < 90:
+#  print("Grade : B")
+#elif score >= 70 <= score and score < 80:
+#  print("Grade : C")
+#elif score >= 60 <= score and score < 70:
+#  print("Grade: D")
+#else:
+#  print("Grade : F")
